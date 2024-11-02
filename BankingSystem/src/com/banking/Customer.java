e43a08dd905b24a28c38d86604f06c8babb626cf
CUBA <mrmukhuba@gmail.com>
2024-11-02 18:31:00 +0200

    Update Customer.java
    Latest updates on Customer file

--- BankingSystem/src/com/banking/Customer.java
+++ BankingSystem/src/com/banking/Customer.java
@@ -1,25 +1,25 @@
-package com.banking;
+package com.bankingapp;
 
 public class Customer {
     private String name;
-    private String id;
     private BankAccount account;
+    private String email;
 
-    public Customer(String name, String id, String accountNumber) {
+    public Customer(String name, String pin, double initialBalance, String email) {
         this.name = name;
-        this.id = id;
-        this.account = new BankAccount(accountNumber);
+        this.account = new BankAccount(pin, initialBalance);
+        this.email = email;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getId() {
-        return id;
-    }
-
     public BankAccount getAccount() {
         return account;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
